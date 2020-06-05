08b6c9ea4c4ae7b2a5fc6cac5123ce5537a0114b
angelusualle <angelusualle@gmail.com>
2020-06-05 15:38:04 -0700

    add best w/o data structure

--- 1.1/has_unique_chars.py
+++ 1.1/has_unique_chars.py
@@ -1,3 +1,4 @@
+# O(n)
 def has_unique_chars(s):
     seen = set()
     for c in s:
@@ -5,3 +6,11 @@ def has_unique_chars(s):
             return False
         seen.add(c)
     return True
+
+# O(nlog(n)) w/o data structures
+""" def has_unique_chars(s):
+    s = sorted(s)
+    for i, c in enumerate(s):
+        if i > 0 and c == s[i-1]:
+            return False
+    return True """
