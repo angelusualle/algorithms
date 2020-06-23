60a9207dde695ccd4275175f6f35f53eb90fdaa9
angelusualle <angelusualle@gmail.com>
2020-06-23 06:47:17 -0700

    Made O(n) truly

--- 1.6/compress.py
+++ 1.6/compress.py
@@ -2,16 +2,18 @@
 def compress(string):
     last_letter = string[0]
     count = 1
-    new_string = ''
+    new_string = []
     for letter in string[1:]:
         if last_letter != letter:
-            new_string += last_letter + str(count)
+            new_string.append(last_letter)
+            new_string.append(str(count))
             last_letter = letter
             count = 1
         else:
             count += 1
-    new_string += last_letter + str(count)
+    new_string.append(last_letter)
+    new_string.append(str(count))
     if len(new_string) < len(string):
-        return new_string
+        return ''.join(new_string)
     else:
         return string
