c76dfc78c3ac7a502364b486dd2defb665a4199b
angelusualle <angelusualle@gmail.com>
2020-08-04 11:50:44 -0700

    generate all permutations of a string complete

--- 8.7-8.8/get_all_permutations_of_string.py
+++ 8.7-8.8/get_all_permutations_of_string.py
@@ -1,22 +1,39 @@
-# O(n!) removing
+# O(n!) time, O(n) space, but faster if duplicates present
 def get_all_permutations_of_string_with_dups(string):
     ans = []
-    get_all_permutations_of_arr(list(string),len(string), len(string), ans)
-    set_data = set([''.join(x) for x in ans])
-    print(set_data)
-    return list(set_data)
+    char_counter = {}
+    for letter in string:
+        if letter in char_counter:
+            char_counter[letter] += 1
+        else:
+            char_counter[letter] = 1
+    get_all_permutations_of_arr_with_dups(list(string), "", char_counter, len(string), ans)
+    return [''.join(x) for x in ans]
+
+def get_all_permutations_of_arr_with_dups(arr, prefix, char_counter, remaining, ans):
+    if remaining == 0:
+        ans.append(prefix)
+        return
+    for k in char_counter:
+        count = char_counter[k]
+        if count > 0:
+            char_counter[k] = count - 1
+            get_all_permutations_of_arr_with_dups(arr,prefix+k, char_counter, remaining - 1, ans)
+            char_counter[k] = count
 
 # O(n!)
 def get_all_permutations_of_string(string):
     ans = []
     get_all_permutations_of_arr(list(string),len(string), len(string), ans)
     return [''.join(x) for x in ans]
+
+    
 def get_all_permutations_of_arr(arr, size, n, ans):
     if (size == 1): 
-        ans.append(arr)
+        ans.append(arr[:])
         return
     for i in range(size): 
-        get_all_permutations_of_arr(arr[:],size-1,n, ans)
+        get_all_permutations_of_arr(arr,size-1,n, ans)
         if size % 2: 
             arr[0], arr[size-1] = arr[size-1], arr[0] 
         else: 
--- 8.7-8.8/get_all_permutations_of_string_test.py
+++ 8.7-8.8/get_all_permutations_of_string_test.py
@@ -3,4 +3,7 @@ from get_all_permutations_of_string import get_all_permutations_of_string, get_a
 
 class Test_Case_Get_All_Permutations_Of_String(unittest.TestCase):
     def test_get_all_permutations_of_string(self):
-        self.assertListEqual(get_all_permutations_of_string_with_dups("tea"), ['tea', 'eta', 'aet', 'eat', 'tea', 'eta'])
+        self.assertListEqual(get_all_permutations_of_string("tea"), ['tea', 'eta', 'ate', 'tae', 'eat', 'aet'])
+    def test_get_all_permutations_of_string_with_dups(self):
+        self.assertListEqual(get_all_permutations_of_string_with_dups("aaa"), ['aaa'])
+        self.assertListEqual(get_all_permutations_of_string_with_dups("teat"), ['ttea', 'ttae', 'teta', 'teat', 'tate', 'taet', 'etta', 'etat', 'eatt', 'atte', 'atet', 'aett'])
