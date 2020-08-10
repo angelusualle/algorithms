1ff8b66ab24d5566972fe7cef95ac61cf2961a29
angelusualle <angelusualle@gmail.com>
2020-08-10 15:24:24 -0700

    find in sorted matrix

---
+++ 10.9/find_in_sorted_matrix.py
@@ -0,0 +1,11 @@
+def find_in_sorted_matrix(matrix, val):
+    i = 0
+    j = len(matrix[0]) - 1
+    while ( i < len(matrix) and j >= 0 ): 
+        if (matrix[i][j] == val ): 
+            return (i,j)
+        if (matrix[i][j] > val ): 
+            j -= 1
+        else:  
+            i += 1
+    return None
