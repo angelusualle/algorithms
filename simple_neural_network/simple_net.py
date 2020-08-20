3b6fa4a3f3398a2667c9bf38796e54f45fb04fee
angelusualle <angelusualle@gmail.com>
2020-08-20 15:39:57 -0700

    adding layer to neural network

--- simple_neural_network/simple_net.py
+++ simple_neural_network/simple_net.py
@@ -6,8 +6,8 @@ data = np.array([[1,1,2],
 
 ans = np.array([[4,12,21]]).T
 
-hidden_nodes = 4
-lr = 100
+hidden_nodes = 1000
+lr = .01
 
 def relu(x):
     return (x > 0)*x
@@ -16,15 +16,20 @@ def relu_deriv(x):
     return x > 0
 
 weights_X_1 = 2*np.random.random((3, hidden_nodes)) - 1
-weights_1_O = 2*np.random.random((hidden_nodes, 1)) - 1
+weights_1_2 = 2*np.random.random((hidden_nodes, hidden_nodes)) - 1
+weights_2_O = 2*np.random.random((hidden_nodes, 1)) - 1
 
-for epoch in range(100):
+
+for epoch in range(1000):
     layer_1 =  relu(data.dot(weights_X_1))
-    layer_2 =  layer_1.dot(weights_1_O)
+    layer_2 =  relu(layer_1.dot(weights_1_2))
+    layer_3 = layer_2.dot(weights_2_O)
     
-    layer_2_delta = layer_2-ans
-    layer_1_delta = layer_2_delta.dot(weights_1_O.T)*(relu_deriv(layer_1))
-    weights_1_O -= lr*layer_1.T.dot(layer_2_delta)
+    layer_3_delta = layer_3-ans
+    layer_2_delta = layer_3_delta.dot(weights_2_O.T)*(relu_deriv(layer_2))
+    layer_1_delta = layer_2_delta.dot(weights_1_2.T)*(relu_deriv(layer_1))
+    weights_2_O -= lr*layer_2.T.dot(layer_3_delta)
+    weights_1_2 -= lr*layer_1.T.dot(layer_2_delta)
     weights_X_1 -= lr*data.T.dot(layer_1_delta)
 
-    print("Epoch %i, loss (mse): %f, mae: %f" % (epoch+1, 0.5*np.sum((layer_2 - ans)**2)/3.0, np.sum(np.abs(layer_2 - ans))))
+    print("Epoch %i, loss (mse): %f, mae: %f" % (epoch+1, 0.5*np.sum((layer_3 - ans)**2)/3.0, np.sum(np.abs(layer_3 - ans))))
