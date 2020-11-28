cc120240191adc4975d27ee62436129ed5d0a7cf
angelusualle <angelusualle@gmail.com>
2020-11-28 08:39:11 -0800

    knapsack asserted

--- general_interview_qs/knapsack/knapsack_test.py
+++ general_interview_qs/knapsack/knapsack_test.py
@@ -5,6 +5,7 @@ class Test_Case_Knapsack(unittest.TestCase):
     def test_knapsack(self):
         ksack = Knapsack(20, [4,2,6,3,1,9], [1,1,2,14,5,6])
         ksack.solve()
+        self.assertEqual(ksack.best_config, '4 2 6 1 9 ')
 
 if __name__ == '__main__':
     unittest.TestCase()
