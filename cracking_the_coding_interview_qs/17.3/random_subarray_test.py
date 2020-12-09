308b106aa53e3d2a651956b80ec35dac36fb464e
angelusualle <angelusualle@gmail.com>
2020-12-09 12:24:46 -0800

    added assertion

--- cracking_the_coding_interview_qs/17.3/random_subarray_test.py
+++ cracking_the_coding_interview_qs/17.3/random_subarray_test.py
@@ -6,7 +6,7 @@ class Test_Case_Random_Subarray(unittest.TestCase):
         arrs = []
         for i in range(100000):
             arrs.append(random_subarray([1,2,3], 2))
-        print(len([x for x in arrs if x[0] == 1 and x[1] == 2]) / len(arrs))
+        self.assertAlmostEqual(len([x for x in arrs if x[0] == 1 and x[1] == 2]) / len(arrs), 0.3333, delta=0.01)
 
 if __name__ == '__main__':
     unittest.main()
