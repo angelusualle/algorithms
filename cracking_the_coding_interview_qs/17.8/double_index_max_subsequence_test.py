a9269a2e2a2bab6af212643a523fa8355e24f2ce
angelusualle <angelusualle@gmail.com>
2020-12-15 11:35:35 -0800

    fixed unit test

--- cracking_the_coding_interview_qs/17.8/double_index_max_subsequence_test.py
+++ cracking_the_coding_interview_qs/17.8/double_index_max_subsequence_test.py
@@ -3,4 +3,4 @@ from double_index_max_subsequence import double_index_max_subsequence
 
 class Test_Case_Double_Index_Max_Subsequence(unittest.TestCase):
     def test_double_index_max_subsequence(self):
-        self.assertEqual("", str(double_index_max_subsequence([(65, 100), (70, 150), (56, 90), (75, 190), (60, 95), (68, 110)])))
+        self.assertEqual("[(56, 90), (60, 95), (65, 100), (68, 110), (70, 150), (75, 190)]", str(double_index_max_subsequence([(65, 100), (70, 150), (56, 90), (75, 190), (60, 95), (68, 110)])))
