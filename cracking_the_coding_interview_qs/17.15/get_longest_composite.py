3be8cf7945d5c4392bbc4f965ca7bb9e0e436392
angelusualle <angelusualle@gmail.com>
2020-12-24 11:09:12 -0800

    fixed for multi component composite word

--- cracking_the_coding_interview_qs/17.15/get_longest_composite.py
+++ cracking_the_coding_interview_qs/17.15/get_longest_composite.py
@@ -10,12 +10,15 @@ def get_longest_composite(words):
 def is_composite(word, word_set, cache, self_ok):
     if word in cache:
         return cache[word]
+    if self_ok and word in word_set:
+        cache[word] = True
+        return True
     for i in range(1, len(word)):
         l = word[:i]
         if l not in word_set:
             continue
         r = word[i:]
-        if r in word_set:
+        if is_composite(r, word_set, cache, self_ok=True):
             cache[word] = True
             return True
     cache[word] = False
