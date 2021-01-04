e5f33a53f5dda2769ba7a7f9f973a3eb79e585e6
angelusualle <angelusualle@gmail.com>
2021-01-04 13:04:52 -0800

    fixed word transform

--- cracking_the_coding_interview_qs/17.22/word_transform.py
+++ cracking_the_coding_interview_qs/17.22/word_transform.py
@@ -1,24 +1,41 @@
-# O(N^3) time O(1) space
-def word_transform(w1, w2, cache, dictionary, t = 0):
-    if w1+w2 in cache:
-        return cache[w1+w2]
-    if w1 == w2:
-        return w2
-    diff = 0
-    for i in range(len(w1)):
-        if w1[i] != w2[i]:
-            diff += 1
-    if diff == 1:
-        return w2
-    for i in range(t, len(w1)):
-        sub = w1[0:i] + w2[i] + w1[i+1:]
-        if sub in dictionary:
-            path = word_transform(sub, w2, cache, dictionary, t+1)
-            if path is not None:
-                cache[w1+w2] = sub + '-> ' + path
-                if t != 0:
-                    return sub + '-> ' + path
-                else:
-                    return w1 + '-> ' + sub + '-> ' + path
-    cache[w1+w2] = None
+class Node():
+    def __init__(self, val):
+        self.val = val
+        self.adjacent_nodes = []
+        self.visited = False
+
+# O(E^d) where d is depth
+def word_transform(w1, w2, dictionary):
+    # make dictionary into linked list
+    start_node = Node(w1)
+    end_node = Node(w2)
+    nodes = [start_node, end_node]
+    nodes_dict = {start_node.val: start_node, end_node.val: end_node}
+    while nodes:
+        node = nodes.pop()
+        word = node.val
+        for w in dictionary:
+            if w == word:
+                continue
+            diff = 0
+            for i in range(len(word)):
+                if w[i] != word[i]:
+                    diff += 1
+            if diff == 1:
+                if w not in nodes_dict:
+                    nodes_dict[w] = Node(w)
+                    nodes.append(nodes_dict[w])
+                node.adjacent_nodes.append(nodes_dict[w])
+    nodes = [start_node]
+    path = []
+    while nodes:
+        node = nodes.pop()
+        if node == end_node:
+            path.append(node.val)
+            return ''.join(path)
+        path.append(node.val + ' -> ')
+        node.visited = True
+        for c in node.adjacent_nodes:
+            if not c.visited:
+                nodes.append(c)
     return None
--- cracking_the_coding_interview_qs/17.22/word_transform_test.py
+++ cracking_the_coding_interview_qs/17.22/word_transform_test.py
@@ -3,4 +3,4 @@ from word_transform import word_transform
 
 class Test_Case_Word_Transform(unittest.TestCase):
     def test_word_transform(self):
-        self.assertEqual(word_transform('DAMP', 'LIKE', {}, set(['DAMP', 'LIKE', 'LAMP', 'LIMP', 'LIME'])), 'DAMP-> LAMP-> LIMP-> LIME-> LIKE')
+        self.assertEqual(word_transform('DAMP', 'LIKE', set(['DAMP', 'LIKE', 'LAMP', 'LIMP', 'LIME'])), 'DAMP -> LAMP -> LIMP -> LIME -> LIKE')
