084d2d8b900ff6919e8fca1d6a72e8a73e950510
angelusualle <angelusualle@gmail.com>
2021-01-12 10:32:47 -0800

    added none case to binary search (python)

--- general_interview_qs/binary_search/find_funcs_test.py
+++ general_interview_qs/binary_search/find_funcs_test.py
@@ -4,5 +4,6 @@ import unittest
 class Test_Case_Find_Funcs(unittest.TestCase):
     def test_find_val(self):
         self.assertEqual(find_val([1,2,3,4,5,6,7], 5), 4)
+        self.assertIsNone(find_val([1,2,3,5,6,7], 4))
     def test_find_first_in_series(self):
         self.assertEqual(find_last_in_series([1,2,3,4,5,6,7], lambda x: x <= 5), 4) 
