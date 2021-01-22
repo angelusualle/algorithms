17588bdff52f9e253ccd181c29481b3cb29c07b8
angelusualle <angelusualle@gmail.com>
2021-01-22 10:28:39 -0800

    Chapter 9 completed tests

--- grokking_algorithms/cpp/ChapterNineTest.h
+++ grokking_algorithms/cpp/ChapterNineTest.h
@@ -10,8 +10,9 @@ class ChapterNineTest:UnitTest{
             std::vector<int> cost{20, 5, 17, 40, 25};
             std::vector<bool> inclusions;
             std::vector<bool> answer = knapsackSolver(itemsWeight, cost, 0, inclusions, 30, 0);
+            std::vector<bool> trueAnswer{true, false, true, true, true};
             for (int i = 0; i < cost.size(); i++){
-                std::cout << answer[i];
+                UT_ASSERT(answer[i] == trueAnswer[i]);
             }
 
         }
