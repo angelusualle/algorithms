cd5e0be52809a58fb46586d002b31c62444966e8
angelusualle <angelusualle@gmail.com>
2021-02-03 11:34:33 -0800

    fix str int problem in python reverse int problem

--- leetcode/python/problem7/reverse_int_test.py
+++ leetcode/python/problem7/reverse_int_test.py
@@ -3,7 +3,7 @@ from reverse_int import reverse_int
 
 class Test_Case_Reverse_Int(unittest.TestCase):
     def test_reverse_int(self):
-        self.assertEqual(reverse_int(321), '123')
+        self.assertEqual(reverse_int(321), 123)
 
 if __name__ == '__main__':
     unittest.main()
